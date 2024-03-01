ad670597923f3be4ded517f33c8b3f4993641c8e
Hurng-Chun Lee <h.lee@donders.ru.nl>
2024-03-01 20:55:51 +0100

    improved error page

--- ui/routes/error.js
+++ ui/routes/error.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var StatusCodes = require('http-status-codes').StatusCodes;
+var ReasonPhrases = require('http-status-codes').ReasonPhrases;
 
 /** simple router for error code page */
 router.get('/403', function(_, _, next) {
     return next({
-        status: 403
+        status: StatusCodes.FORBIDDEN,
+        message: ReasonPhrases.FORBIDDEN
     });
 });
 
