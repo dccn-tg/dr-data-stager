f0bbb45119ae88d2ec44c96e18c21ae5ac5f5578
Hurng-Chun Lee <h.lee@donders.ru.nl>
2024-03-01 13:28:53 +0100

    rearranged docker-compose files

--- env.sh
+++ env.sh
@@ -4,6 +4,9 @@ PROJECT_VOL=/tmp/project
 PROJECT_CEPHFS_VOL=/tmp/project_cephfs
 PROJECT_FREENAS_VOL=/tmp/project_freenas
 
+# stager task persistent store
+TASK_DB_REDIS_DATA=/tmp/data
+
 # configuration files
 API_CONFIG=$(pwd)/config/api-server.yml
 WORKER_CONFIG=$(pwd)/config/worker.yml
