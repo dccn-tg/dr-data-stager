97e0d81bc27e59664ab9c7ec2f93007a8ce4b3fd
Hurng-Chun Lee <h.lee@donders.ru.nl>
2024-03-01 23:51:20 +0100

    removed unused function

--- ui/routes/auth.js
+++ ui/routes/auth.js
@@ -15,28 +15,4 @@ const _isAuthenticated = function(req, _, next) {
     }
 }
 
-// Middleware to verify admin credentials via authorization header
-var _verifyAdminCredentials = function(req, _, next) {
-    try {
-        const adminUsername = req.app.locals.STREAMER_UI_DB_USER;
-        const adminPassword = req.app.locals.STREAMER_UI_DB_PASSWORD;
-    
-        if ( ! req.headers.authorization ) throw new Error("no admin user credentials");
-    
-        const base64Credentials = req.headers.authorization.split(' ')[1];
-        const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-        idx = credentials.indexOf(':');
-        const username = credentials.substring(0,idx);
-        const password = credentials.substring(idx+1, credentials.length);
-    
-        if (username !== adminUsername || password !== adminPassword) {
-            throw new Error("Invalid admin user credentials");
-        }
-        next();
-    } catch(err) {
-        next(createError(401, err));
-    }
-}
-
 module.exports.isAuthenticated = _isAuthenticated;
-module.exports.verifyAdminCredentials = _verifyAdminCredentials;
